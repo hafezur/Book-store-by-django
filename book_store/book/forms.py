baf298f3dcf4be7fc64b80bcb44546b1c896b892
Hafezur Rahman <hafezurrahman360@gmail.com>
2025-10-22 10:10:40 +0600

    initial commit

--- book_store/book/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms 
-
-from book.models import BookStoreModel
-
-class BookStoreForm(forms.ModelForm):
-    class Meta:
-        model=BookStoreModel
-        fields=['id','book_name','author','category']
